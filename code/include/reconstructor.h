e6ac84aa33b5419585ac064265f241d8bb0751c2
Vavrines <tianbaixiao@gmail.com>
2020-03-30 09:28:02 +0200

    added initial reconstruction structure

---
+++ code/include/reconstructor.h
@@ -0,0 +1,26 @@
+#ifndef RECONSTRUCTOR_H
+#define RECONSTRUCTOR_H
+
+#include "typedef.h"
+
+class Reconstructor
+{
+public:
+    /**
+     * @brief Reconstruction
+     * @param settings
+     */
+    Reconstructor(Settings* settings);
+
+    /**
+     * @brief Flux computes flux on edge for fixed ordinate at a given edge
+     * @param Omega fixed ordinate for flux computation
+     * @param psiL left solution state
+     * @param psiR right solution state
+     * @param n scaled normal vector of given edge
+     * @return reconstructed slope
+     */
+    virtual double Slope(const Vector& Omega, double psiL, double psiR, const Vector& n)const;
+};
+
+#endif // RECONSTRUCTOR_H
---
+++ code/src/reconstructor.cpp
@@ -0,0 +1,11 @@
+#include "reconstructor.h"
+
+Reconstructor::Reconstructor(Settings* settings)
+{
+
+}
+
+double Reconstructor::Slope(const Vector& Omega, double psiL, double psiR, const Vector& n)const{
+
+    return 0.0;
+}
