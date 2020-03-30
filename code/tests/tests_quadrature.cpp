81a5bd8182899fe288cdff537d89db785677a0f6
Thomas <thomas@camminady.org>
2020-03-30 13:25:24 +0200

    Use auto keyword in quadrature tests.

--- code/tests/tests_quadrature.cpp
+++ code/tests/tests_quadrature.cpp
@@ -2,6 +2,7 @@
 #include "quadrature.h"
 
 vector<std::string> quadraturenames = {"montecarlo"};
+vector<int> quadratureorders        = {4, 5, 6, 7};
 
 bool approxequal( double a, double b ) {
     double tol = 1e-15;
@@ -10,8 +11,8 @@ bool approxequal( double a, double b ) {
 
 TEST_CASE( "Quadrature weights sum to 4*pi.", "WHAT TO PUT HERE?" ) {
     for( auto quadraturename : quadraturenames ) {
-        for( int order = 4; order < 8; order++ ) {
-            Quadrature* Q = Quadrature::CreateQuadrature( quadraturename, order );
+        for( auto quadratureorder : quadratureorders ) {
+            Quadrature* Q = Quadrature::CreateQuadrature( quadraturename, quadratureorder );
             REQUIRE( approxequal( 4 * M_PI, Q->SumUpWeights() ) );
         }
     }
@@ -19,8 +20,8 @@ TEST_CASE( "Quadrature weights sum to 4*pi.", "WHAT TO PUT HERE?" ) {
 
 TEST_CASE( "Quadrature points are on the unit sphere.", "WHAT TO PUT HERE?" ) {
     for( auto quadraturename : quadraturenames ) {
-        for( int order = 4; order < 8; order++ ) {
-            Quadrature* Q = Quadrature::CreateQuadrature( quadraturename, order );
+        for( auto quadratureorder : quadratureorders ) {
+            Quadrature* Q = Quadrature::CreateQuadrature( quadraturename, quadratureorder );
 
             blaze::DynamicVector<blaze::DynamicVector<double>> points = Q->GetPoints();
             for( int i = 0; i < Q->GetNq(); i++ ) {
@@ -30,11 +31,22 @@ TEST_CASE( "Quadrature points are on the unit sphere.", "WHAT TO PUT HERE?" ) {
     }
 }
 
-TEST_CASE( "Nq is actually equal to the weight vector length.", "WHAT TO PUT HERE?" ) {
+/*
+TEST_CASE( "Nq is actually equal to the number of weights.", "WHAT TO PUT HERE?" ) {
     for( auto quadraturename : quadraturenames ) {
-        for( int order = 4; order < 8; order++ ) {
-            // Quadrature* Q = Quadrature::CreateQuadrature( quadraturename, order );
-            // REQUIRE( Q->GetNq() == size( Q->GetWeights() ) );
+        for( auto quadratureorder : quadratureorders ) {
+            Quadrature* Q = Quadrature::CreateQuadrature( quadraturename, quadratureorder );
+            REQUIRE( Q->GetNq() == size( Q->GetWeights() ) );
         }
     }
 }
+
+TEST_CASE( "Nq is actually equal to the number of points.", "WHAT TO PUT HERE?" ) {
+    for( auto quadraturename : quadraturenames ) {
+        for( auto quadratureorder : quadratureorders ) {
+            Quadrature* Q = Quadrature::CreateQuadrature( quadraturename, quadratureorder );
+            REQUIRE( Q->GetNq() == size( Q->GetPoints() ) );
+        }
+    }
+}
+*/
