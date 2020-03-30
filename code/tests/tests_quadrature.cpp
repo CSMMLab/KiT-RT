77c8499c84fb05a419cee8f74ed736aa8f23a956
Thomas <thomas@camminady.org>
2020-03-30 11:52:31 +0200

    Check that quadrature weights sum to 4pi.

--- code/tests/tests_quadrature.cpp
+++ code/tests/tests_quadrature.cpp
@@ -1,11 +1,17 @@
 #include "catch.hpp"
 
-#include "factorial_example.h"
+#include "quadrature.h"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial( 0 ) == 1 );
-    REQUIRE( Factorial( 1 ) == 1 );
-    REQUIRE( Factorial( 2 ) == 2 );
-    REQUIRE( Factorial( 3 ) == 6 );
-    REQUIRE( Factorial( 10 ) == 3628800 );
+bool approxequal( double a, double b ) {
+    double tol = 1e-15;
+    return abs( a - b ) < tol;
 }
+
+TEST_CASE( "Quadrature weights sum to 4*pi.", "WHAT TO PUT HERE?" ) {
+    for( int order = 4; order < 8; order++ ) {
+        Quadrature* Q = Quadrature::CreateQuadrature( "montecarlo", order );
+        REQUIRE( approxequal( 4 * M_PI, Q->SumUpWeights() ) );
+    }
+}
+
+TEST_CASE( "Quadrature points are on the unit sphere.", "WHAT TO PUT HERE?" ) {}
