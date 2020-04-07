f74f1ddb75563e5f3d333392fd3ea78e9791c85c
vavrines <tianbaixiao@gmail.com>
2020-04-07 22:37:59 +0200

    revised function name

--- code/src/reconstructor.cpp
+++ code/src/reconstructor.cpp
@@ -28,7 +28,7 @@ double LMinMod(double sL, double sR){
 
 }
 
-double LvanLeer(double sL, double sR){
+double LVanLeer(double sL, double sR){
 
     return (FortSign(1.0, sL) + FortSign(1.0, sR)) * abs(sL) * abs(sR) / (abs(sL) + abs(sR) + 0.0000001);
 
