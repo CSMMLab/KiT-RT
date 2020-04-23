dc63188c03c9df5c43b721e07da938817468ecfe
vavrines <tianbaixiao@gmail.com>
2020-04-23 10:19:28 +0200

    updated reconstructor

--- code/include/reconstructor.h
+++ code/include/reconstructor.h
@@ -13,7 +13,7 @@ class Reconstructor
      */
     Reconstructor( Settings* settings );
 
-    /**
+    /** Method 1: structured developing
      * @brief Slope of angular flux psi inside a given cell
      * @param Omega fixed ordinate for flux computation
      * @param psiL left solution state
@@ -21,7 +21,19 @@ class Reconstructor
      * @param n scaled normal vector of given edge
      * @return reconstructed slope
      */
-    virtual double Slope( const Vector& Omega, double psiL, double psiR, const Vector& n ) const;
+
+    /** Method 2: unstructured developing
+     * @brief Slope of angular flux psi inside a given cell
+     * @param Omega fixed ordinate for flux computation
+     * @param psiL left solution state
+     * @param psiR right solution state
+     * @param n scaled normal vector of given edge
+     * @return reconstructed slope
+     */
+
+    virtual double ReconstructSlopeStruct( double uL, double uC, double uR, double dxL, double dxR, std::string limiter ) const;
+    virtual void ReconstructSlopeUnstruct( unsigned nq, unsigned ncell, VectorVector& psi, VectorVector& psiDerX, VectorVector& psiDerY, Vector& area, VectorVector& neighbor, VectorVector& nx, VectorVector& ny ) const;
+
 };
 
-#endif    // RECONSTRUCTOR_H
+#endif // RECONSTRUCTOR_H
