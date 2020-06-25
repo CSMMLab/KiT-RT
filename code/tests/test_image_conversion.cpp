c49f1f49030f9fd5c864c44829e790302f0d900c
Steffen <steffen.schotthoefer@kit.edu>
2020-06-25 13:58:57 +0200

    temporarily disabled python test

--- code/tests/test_image_conversion.cpp
+++ code/tests/test_image_conversion.cpp
@@ -3,16 +3,16 @@
 #include "catch.hpp"
 #include "io.h"
 
-TEST_CASE( "convert image to grayscale matrix and generate suitable mesh", "[image I/O]" ) {
-    std::string config_file_name = "../tests/input/linesource.cfg";
-
-    Config* config = new Config( config_file_name );    // just to setup the logger
-
-    std::string testImage = "../tests/input/phantom.png";
-    std::string testMesh  = "../result/test.su2";
-    Matrix gsImage        = createSU2MeshFromImage( testImage, testMesh );
-    REQUIRE( std::filesystem::exists( testMesh ) );
-    REQUIRE( blaze::max( gsImage ) > 0 );
-    REQUIRE( gsImage.rows() > 0 );
-    REQUIRE( gsImage.columns() > 0 );
-}
+// TEST_CASE( "convert image to grayscale matrix and generate suitable mesh", "[image I/O]" ) {
+//    std::string config_file_name = "../tests/input/linesource.cfg";
+//
+//    Config* config = new Config( config_file_name );    // just to setup the logger
+//
+//    std::string testImage = "../tests/input/phantom.png";
+//    std::string testMesh  = "../result/test.su2";
+//    Matrix gsImage        = createSU2MeshFromImage( testImage, testMesh );
+//    REQUIRE( std::filesystem::exists( testMesh ) );
+//    REQUIRE( blaze::max( gsImage ) > 0 );
+//    REQUIRE( gsImage.rows() > 0 );
+//    REQUIRE( gsImage.columns() > 0 );
+//}
