e0cec2fe3e4fd43751007f0d63c247e688b08d84
kx5574@kit.edu <kx5574@uc2n996.localdomain>
2020-08-07 17:08:35 +0200

    created jobscript for GPU runs
    Former-commit-id: 1fa285354283dd46c401182530a55a4fc4258138

---
+++ code/python/neural_entropy/run_job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu_4
+#SBATCH --time=24:00:00
+#SBATCH --mem=200gb
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=EntropyTrainer
+
+python3 trainNNMK2.py
