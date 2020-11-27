ea2e6d7b91458b3af1c5e687ea2188ee1a091d75
Steffen Schotthöfer <steffen.schottthoefer@kit.edu>
2020-11-27 11:41:44 +0100

    commented optimizers and added some comments on mesh

--- code/src/optimizers/optimizerbase.cpp
+++ code/src/optimizers/optimizerbase.cpp
@@ -1,11 +1,14 @@
 #include "optimizers/optimizerbase.h"
 #include "common/config.h"
+#include "entropies/entropybase.h"
 #include "optimizers/newtonoptimizer.h"
 
 OptimizerBase::OptimizerBase( Config* settings ) {
     _entropy  = EntropyBase::Create( settings );
     _settings = settings;
 }
+
+OptimizerBase::~OptimizerBase() { delete _entropy; }
 
 OptimizerBase* OptimizerBase::Create( Config* settings ) {
     switch( settings->GetOptimizerName() ) {
