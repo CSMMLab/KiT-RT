f8118df4a31eb193dc3f2791d39e93561b84c4a3
pia.stammer <pia.stammer@kit.edu>
2020-11-27 15:19:24 +0100

    Added comments to problem files

--- code/include/problems/checkerboard.h
+++ code/include/problems/checkerboard.h
@@ -6,13 +6,13 @@
 class Checkerboard_SN : public ProblemBase
 {
   private:
-    Vector _scatteringXS;
-    Vector _totalXS;
+    Vector _scatteringXS;     /*! @brief Vector of scattering crosssections */
+    Vector _totalXS;      /*! @brief Vector of total crosssections */
 
     Checkerboard_SN() = delete;
 
-    bool isAbsorption( const Vector& pos ) const;
-    bool isSource( const Vector& pos ) const;
+    bool isAbsorption( const Vector& pos ) const; /*! @return True if pos is in absorption region, False otherwise */
+    bool isSource( const Vector& pos ) const; /*! @return True if pos is in source region, False otherwise */
 
   public:
     Checkerboard_SN( Config* settings, Mesh* mesh );
@@ -27,13 +27,22 @@ class Checkerboard_SN : public ProblemBase
 class Checkerboard_PN : public ProblemBase
 {
   private:
-    Vector _scatteringXS;
-    Vector _totalXS;
+    Vector _scatteringXS;     /*! @brief Vector of scattering crosssections */
+    Vector _totalXS;      /*! @brief Vector of total crosssections */
 
     Checkerboard_PN() = delete;
 
-    bool isAbsorption( const Vector& pos ) const;
-    bool isSource( const Vector& pos ) const;
+    bool isAbsorption( const Vector& pos ) const; /*! @return True if pos is in absorption region, False otherwise */
+    bool isSource( const Vector& pos ) const; /*! @return True if pos is in source region, False otherwise */
+
+    /**
+     * @brief Gets the global index for given order l of Legendre polynomials and given
+     *        order k of Legendre functions.
+     *        Note: This is code doubling from PNSolver::GlobalIndex
+     * @param l : order of Legendre polynomial
+     * @param k : order of Legendre function
+     * @returns global index
+     */
     int GlobalIndex( int l, int k ) const;
 
   public:
