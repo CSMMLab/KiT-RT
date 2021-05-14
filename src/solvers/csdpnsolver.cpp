5a6d68b456cadd8d4210a824b2c213a236f8556a
Jonas Kusch <jonas.kusch@kit.edu>
2021-05-14 12:29:31 +0200

    density division in flux

--- src/solvers/csdpnsolver.cpp
+++ src/solvers/csdpnsolver.cpp
@@ -104,6 +104,75 @@ void CSDPNSolver::IterPostprocessing( unsigned idx_iter ) {
     }
 }
 
+void CSDPNSolver::FluxUpdate() {
+    if( _reconsOrder > 1 ) {
+        _mesh->ReconstructSlopesU( _nSystem, _solDx, _solDy, _sol );    // unstructured reconstruction
+        //_mesh->ComputeSlopes( _nTotalEntries, _solDx, _solDy, _sol );    // unstructured reconstruction
+    }
+    // Vector solL( _nTotalEntries );
+    // Vector solR( _nTotalEntries );
+    auto solL = _sol[2];
+    auto solR = _sol[2];
+
+    // Loop over all spatial cells
+    for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
+
+        // Dirichlet cells stay at IC, farfield assumption
+        if( _boundaryCells[idx_cell] == BOUNDARY_TYPE::DIRICHLET ) continue;
+
+        // Reset temporary variable psiNew
+        for( unsigned idx_sys = 0; idx_sys < _nSystem; idx_sys++ ) {
+            _solNew[idx_cell][idx_sys] = 0.0;
+        }
+
+        // Loop over all neighbor cells (edges) of cell j and compute numerical fluxes
+        for( unsigned idx_neighbor = 0; idx_neighbor < _neighbors[idx_cell].size(); idx_neighbor++ ) {
+
+            // Compute flux contribution and store in psiNew to save memory
+            if( _boundaryCells[idx_cell] == BOUNDARY_TYPE::NEUMANN && _neighbors[idx_cell][idx_neighbor] == _nCells )
+                _solNew[idx_cell] += _g->Flux( _AxPlus,
+                                               _AxMinus,
+                                               _AyPlus,
+                                               _AyMinus,
+                                               _AzPlus,
+                                               _AzMinus,
+                                               _sol[idx_cell] / _density[idx_cell],
+                                               _sol[idx_cell] / _density[idx_cell],
+                                               _normals[idx_cell][idx_neighbor] );
+            else {
+                switch( _reconsOrder ) {
+                    // first order solver
+                    case 1:
+                        _solNew[idx_cell] += _g->Flux( _AxPlus,
+                                                       _AxMinus,
+                                                       _AyPlus,
+                                                       _AyMinus,
+                                                       _AzPlus,
+                                                       _AzMinus,
+                                                       _sol[idx_cell] / _density[idx_cell],
+                                                       _sol[_neighbors[idx_cell][idx_neighbor]] / _density[_neighbors[idx_cell][idx_neighbor]],
+                                                       _normals[idx_cell][idx_neighbor] );
+                        break;
+                    // second order solver
+                    case 2:
+
+                    // default: first order solver
+                    default:
+                        _solNew[idx_cell] += _g->Flux( _AxPlus,
+                                                       _AxMinus,
+                                                       _AyPlus,
+                                                       _AyMinus,
+                                                       _AzPlus,
+                                                       _AzMinus,
+                                                       _sol[idx_cell] / _density[idx_cell],
+                                                       _sol[_neighbors[idx_cell][idx_neighbor]] / _density[_neighbors[idx_cell][idx_neighbor]],
+                                                       _normals[idx_cell][idx_neighbor] );
+                }
+            }
+        }
+    }
+}
+
 void CSDPNSolver::FVMUpdate( unsigned idx_energy ) {
 // loop over all spatial cells
 #pragma omp parallel for
