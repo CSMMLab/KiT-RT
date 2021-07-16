1aa586e552746780b997feeb47b0a451f8c8c143
Steffen Schotthöfer <steffen.schottthoefer@kit.edu>
2021-07-16 22:15:02 +0200

    made ComputeHessioan public, since it is needed in datagenerator

--- include/optimizers/newtonoptimizer.h
+++ include/optimizers/newtonoptimizer.h
@@ -25,18 +25,19 @@ class NewtonOptimizer : public OptimizerBase
                 grad = <eta(alpha*m)> - alpha*sol */
     double ComputeObjFunc( Vector& alpha, Vector& sol, const VectorVector& moments );
 
+    /*! @brief Computes hessian of objective function and stores it in hessian
+        grad = <mXm*eta*'(alpha*m)> */
+    void ComputeHessian( Vector& alpha, const VectorVector& moments, Matrix& hessian );
+
   private:
     /*! @brief Computes gradient of objective function and stores it in grad
                 grad = <m*eta*'(alpha*m)> - sol */
     void ComputeGradient( Vector& alpha, Vector& sol, const VectorVector& moments, Vector& grad );
 
-    /*! @brief Computes hessian of objective function and stores it in hessian
-                grad = <mXm*eta*'(alpha*m)> */
-    void ComputeHessian( Vector& alpha, const VectorVector& moments, Matrix& hessian );
-
     QuadratureBase* _quadrature; /*!< @brief used quadrature */    // THis is memory doubling! Try to use a pointer.
-    unsigned _nq;                                                  /*!< @brief number of quadrature points */
-    Vector _weights;                                               /*!<  @brief quadrature weights, dim(_weights) = (_nq) */
+
+    unsigned _nq;    /*!< @brief number of quadrature points */
+    Vector _weights; /*!<  @brief quadrature weights, dim(_weights) = (_nq) */
 
     double _epsilon;                 /*!< @brief Termination criterion for newton optimizer */
     unsigned short _maxIterations;   /*!< @brief Max iterations of the newton solver */
