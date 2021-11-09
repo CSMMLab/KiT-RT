ef957f6727c4c76054287de5bea79d6e9a73220e
chinsp <upmho@student.kit.edu>
2021-11-09 10:37:11 +0100

    fixed override conflict

--- include/solvers/firstcollisionsnsolver.h
+++ include/solvers/firstcollisionsnsolver.h
@@ -10,7 +10,7 @@
 class QuadratureBase;
 class ScatteringKernel;
 
-class FirstCollisionSNSolver : public SolverBase ( settings ) {
+class FirstCollisionSNSolver : public SolverBase {
 
 public:
     /*! @brief Constructor of FirstCollisonSNSolver */
@@ -26,7 +26,7 @@ private:
     /*! @brief Performs preprocessing for the current solver iteration */
     void IterPreprocessing( unsigned idx_pseudotime ) override;
     /*! @brief Performs postprocessing for the current solver iteration */
-    void IterPostprocessing( ) override;
+    void IterPostprocessing( unsigned idx_iter ) override;
     /*! @brief: Computes the flux of the solution to check conservation properties */
     void ComputeRadFlux() override;
 
