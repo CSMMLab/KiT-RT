68e560be608cc02ebdcf8d4db06af0f0c44bf390
chinsp <upmho@student.kit.edu>
2021-11-09 10:44:57 +0100

    Made firstcollisionsnsolver part of SolverBase

--- src/solvers/firstcollisionsnsolver.cpp
+++ src/solvers/firstcollisionsnsolver.cpp
@@ -25,7 +25,7 @@
 #include <stdio.h>
 
 
-FirstCollisionSNSolver::FirstCollisionSNSolver( Config * settings ) : Solver ( settings ){
+FirstCollisionSNSolver::FirstCollisionSNSolver( Config * settings ) : SolverBase ( settings ){
 
     localizedIC = false; // Source only in positive x-direction
 
