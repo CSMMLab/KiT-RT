d245f322a922e61b50df7ad00ca8f76716f9663f
ScSteffen <steffen.schotthoefer@outlook.de>
2022-01-27 15:12:54 +0100

    dockerfiles now with cpu-specifications

---
+++ tools/CI/docker_run_interactive_specify_cores.sh
@@ -0,0 +1,1 @@
+docker run -i -t --rm --cpuset-cpus=0-1 -v $(pwd)/../..:/mnt kitrt-ml-cpu /bin/bash  
--- tools/CI/install_kitrt.sh
+++ tools/CI/install_kitrt.sh
@@ -5,6 +5,6 @@ git submodule update --init --recursive
 mkdir build
 cd build
 rm -rf *
-cmake -G Ninja -DCMAKE_BUILD_UNITY=FALSE -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=False -DBUILD_ML=True ../
+cmake -G Ninja -DCMAKE_BUILD_UNITY=False -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=False -DBUILD_ML=True ../
 ninja
 
