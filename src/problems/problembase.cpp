f24df8b8923b486dd5dcc6254997a00307f2ddc7
Steffen <steffen.schotthoefer@kit.edu>
2022-02-28 17:17:36 +0100

    fix segmentation fault for moment IC in radiationCT image

--- src/problems/problembase.cpp
+++ src/problems/problembase.cpp
@@ -84,12 +84,13 @@ ProblemBase* ProblemBase::Create( Config* settings, Mesh* mesh ) {
 std::vector<double> ProblemBase::GetDensity( const VectorVector& cellMidPoints ) { return std::vector<double>( cellMidPoints.size(), 1.0 ); }
 
 // Legacy code: Scattering crossection loaded from database ENDF with physics class -> later overwritten with ICRU data
-VectorVector ProblemBase::GetScatteringXSE( const Vector& energies, const Vector& angles ) {
+VectorVector ProblemBase::GetScatteringXSE( const Vector& /*energies*/, const Vector& /*angles*/ ) {
     ErrorMessages::Error( "Not yet implemented", CURRENT_FUNCTION );
+    return VectorVector( 1, Vector( 1, 0 ) );
 }
 
 // Stopping powers from phyics class or default = -1
-Vector ProblemBase::GetStoppingPower( const Vector& energies ) {
+Vector ProblemBase::GetStoppingPower( const Vector& /* energies */ ) {
     ErrorMessages::Error( "Not yet implemented", CURRENT_FUNCTION );
     return Vector( 1, -1.0 );
 }
--- src/problems/radiationctimage.cpp
+++ src/problems/radiationctimage.cpp
@@ -4,6 +4,7 @@
 #include "common/mesh.hpp"
 #include "toolboxes/errormessages.hpp"
 #include "toolboxes/interpolation.hpp"
+#include "velocitybasis/sphericalbase.hpp"
 
 #include <fstream>
 
@@ -95,28 +96,68 @@ RadiationCTImage_Moment::RadiationCTImage_Moment( Config* settings, Mesh* mesh )
 RadiationCTImage_Moment::~RadiationCTImage_Moment() {}
 
 std::vector<VectorVector> RadiationCTImage_Moment::GetExternalSource( const Vector& energies ) {
-    auto zeroVec = Vector( _settings->GetNQuadPoints(), 0.0 );
-    auto uniform = std::vector<Vector>( _mesh->GetNumCells(), zeroVec );
-    auto Q       = std::vector<VectorVector>( energies.size(), uniform );
-    // ErrorMessages::Error( "Function not yet implemented.", CURRENT_FUNCTION );
+    SphericalBase* tempBase  = SphericalBase::Create( _settings );
+    unsigned ntotalEquations = tempBase->GetBasisSize();
+    delete tempBase;
+
+    Vector zeroVec              = Vector( ntotalEquations, 0.0 );
+    VectorVector uniform        = VectorVector( _mesh->GetNumCells(), zeroVec );
+    std::vector<VectorVector> Q = std::vector<VectorVector>( energies.size(), uniform );
     return Q;
 }
 
 VectorVector RadiationCTImage_Moment::SetupIC() {
-    VectorVector psi( _mesh->GetNumCells(), Vector( _settings->GetNQuadPoints(), 1e-10 ) );
-    auto cellMids         = _mesh->GetCellMidPoints();
-    double enterPositionX = 0.5 * 10;    // 0.0;
-    double enterPositionY = 0.5 * 10;
+    // Compute number of equations in the system
 
-    // Case 2: Ingoing radiation as Gauss curve
-    double t = 1e-5;    // pseudo time for gaussian smoothing
-    for( unsigned j = 0; j < cellMids.size(); ++j ) {
-        double x = cellMids[j][0] - enterPositionX;
-        double y = cellMids[j][1] - enterPositionY;
-        psi[j]   = Vector( _settings->GetNQuadPoints(), 1.0 / ( 4.0 * M_PI * t ) * std::exp( -( x * x + y * y ) / ( 4 * t ) ) );
+    // In case of PN, spherical basis is per default SPHERICAL_HARMONICS
+    SphericalBase* tempBase  = SphericalBase::Create( _settings );
+    unsigned ntotalEquations = tempBase->GetBasisSize();
+
+    VectorVector initial_sol( _mesh->GetNumCells(), Vector( ntotalEquations, 0 ) );    // zero could lead to problems?
+    VectorVector cellMids = _mesh->GetCellMidPoints();
+
+    Vector uIC( ntotalEquations, 0 );
+
+    if( _settings->GetSphericalBasisName() == SPHERICAL_MONOMIALS ) {
+        QuadratureBase* quad          = QuadratureBase::Create( _settings );
+        VectorVector quadPointsSphere = quad->GetPointsSphere();
+        Vector w                      = quad->GetWeights();
+
+        double my, phi;
+        VectorVector moments = VectorVector( quad->GetNq(), Vector( tempBase->GetBasisSize(), 0.0 ) );
+
+        for( unsigned idx_quad = 0; idx_quad < quad->GetNq(); idx_quad++ ) {
+            my                = quadPointsSphere[idx_quad][0];
+            phi               = quadPointsSphere[idx_quad][1];
+            moments[idx_quad] = tempBase->ComputeSphericalBasis( my, phi );
+        }
+        // Integrate <1*m> to get factors for monomial basis in isotropic scattering
+        for( unsigned idx_quad = 0; idx_quad < quad->GetNq(); idx_quad++ ) {
+            uIC += w[idx_quad] * moments[idx_quad];
+        }
+        delete quad;
     }
+
+    // Initial condition is dirac impulse at (x,y) = (0,0) ==> constant in angle ==> all moments - exept first - are zero.
+    double t       = 3.2e-4;    // pseudo time for gaussian smoothing (Approx to dirac impulse)
+    double epsilon = 1e-4;      // minimal value for first moment to avoid div by zero error
+
+    for( unsigned j = 0; j < cellMids.size(); ++j ) {
+        double x = cellMids[j][0];
+        double y = cellMids[j][1];    // (x- 0.5) * (x- 0.5)
+
+        double kinetic_density = std::max( 1.0 / ( 4.0 * M_PI * t ) * std::exp( -( x * x + y * y ) / ( 4 * t ) ), epsilon );
+
+        if( _settings->GetSphericalBasisName() == SPHERICAL_MONOMIALS ) {
+            initial_sol[j] = kinetic_density * uIC / uIC[0];    // Remember scaling
+        }
+        if( _settings->GetSphericalBasisName() == SPHERICAL_HARMONICS ) {
+            initial_sol[j][0] = kinetic_density;
+        }
+    }
+    delete tempBase;    // Only temporally needed
     std::cout << "not correct iC. but gets overwritten in csdpn_jl constructor\n";
-    return psi;
+    return initial_sol;
 }
 
 std::vector<double> RadiationCTImage_Moment::GetDensity( const VectorVector& /*cellMidPoints*/ ) {
