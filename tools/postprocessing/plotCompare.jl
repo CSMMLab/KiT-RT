e719a7a4293c9671beae5643f243f2d9f2cdf12f
Jonas Kusch <jonas.kusch@kit.edu>
2022-03-22 09:50:10 +0100

    updated plotting script

--- tools/postprocessing/plotCompare.jl
+++ tools/postprocessing/plotCompare.jl
@@ -40,14 +40,16 @@ nx2 = Int(floor(sqrt(length(x))))
 xGrid2 = y[1:nx2]
 dose2 = Vec2Mat(nx2,nx2,result2[:,3]);
 
+println("maximums are: left ",maximum(dose1),"; right ",maximum(dose2))
+
 X2 = (xGrid2'.*ones(nx2))
 Y2 = (xGrid2'.*ones(nx2))'
 
 fig, (ax1, ax2,ax3) = plt.subplots(1, 3,figsize=(30,10),dpi=100)
 ax1.pcolormesh(Y1,X1,dose1,vmin=0.0,cmap="magma")
 ax2.pcolormesh(Y2,X2,dose2,vmin=0.0,cmap="magma")
-ax3.plot(xGrid1,dose1[:,Int(floor(length(xGrid1)/2))]./maximum(dose1[:,Int(floor(length(xGrid1)/2))]), "b--", linewidth=2, label="left", alpha=0.8)
-ax3.plot(xGrid2,dose2[:,Int(floor(length(xGrid2)/2))]./maximum(dose2[:,Int(floor(length(xGrid2)/2))]), "r-", linewidth=2, label="right", alpha=0.8)
+ax3.plot(xGrid1,dose1[:,Int(floor(length(xGrid1)/2))]./maximum(dose1[:,Int(floor(length(xGrid1)/2))]), "b--", linewidth=2, label="left, $(input1)", alpha=0.8)
+ax3.plot(xGrid2,dose2[:,Int(floor(length(xGrid2)/2))]./maximum(dose2[:,Int(floor(length(xGrid2)/2))]), "r-", linewidth=2, label="right, $(input2)", alpha=0.8)
 ax3.legend(loc="upper left")
 ax3.set_xlim([xGrid1[1],xGrid1[end]])
 #ax.set_ylim([0,1.05])
