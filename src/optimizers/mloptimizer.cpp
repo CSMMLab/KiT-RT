7387870be970243b3331f0f87e167f3bc3ca3b20
ScSteffen <steffen.schotthoefer@outlook.de>
2022-04-11 23:47:04 +0200

    added configuration for high order models

--- src/optimizers/mloptimizer.cpp
+++ src/optimizers/mloptimizer.cpp
@@ -68,7 +68,7 @@ MLOptimizer::MLOptimizer( Config* settings ) : OptimizerBase( settings ) {
     // Specify model input name
     // Call Model (change call depending on model mk) (Seems to be randomly assigned by tensorflow)
     _tfModelInputName = "";
-    // std::cout << tfModelName << "\n";
+    //std::cout << tfModelName << "\n";
     // if( tfModelName.compare( "Monomial_Mk11_M1_2D" ) == 0 || tfModelName.compare( "Monomial_Mk12_M1_2D" ) == 0 ||
     //    tfModelName.compare( "Monomial_Mk12_M1_2D_gamma3" ) == 0 || tfModelName.compare( "Monomial_Mk13_M3_2D" ) == 0 ) {
     //    _tfModelInputName = "serving_default_input_1:0";
@@ -111,7 +111,15 @@ MLOptimizer::MLOptimizer( Config* settings ) : OptimizerBase( settings ) {
                                                      "Monomial_Mk12_M4_2D",
                                                      "Monomial_Mk12_M4_2D_gamma1",
                                                      "Monomial_Mk12_M4_2D_gamma2",
-                                                     "Monomial_Mk12_M4_2D_gamma3"};
+                                                     "Monomial_Mk12_M4_2D_gamma3",
+                                                     "Monomial_Mk11_M5_2D",
+                                                     "Monomial_Mk11_M5_2D_gamma1",
+                                                     "Monomial_Mk11_M5_2D_gamma2",
+                                                     "Monomial_Mk11_M5_2D_gamma3",
+                                                     "Monomial_Mk12_M5_2D",
+                                                     "Monomial_Mk12_M5_2D_gamma1",
+                                                     "Monomial_Mk12_M5_2D_gamma2",
+                                                     "Monomial_Mk12_M5_2D_gamma3"};
     for( std::vector<std::string>::iterator it = input_name_1_models.begin(); it != input_name_1_models.end(); ++it ) {
         if( tfModelName.compare( *it ) == 0 ) {
             model_found       = true;
@@ -129,7 +137,7 @@ MLOptimizer::MLOptimizer( Config* settings ) : OptimizerBase( settings ) {
     }
 
     if( !model_found ) {
-        ErrorMessages::Error( "Model input name unknown. Use Tensorflow CLI to determine input name and add it to source code", CURRENT_FUNCTION );
+        ErrorMessages::Error( "Model input name <" +tfModelName + "> unknown. Use Tensorflow CLI to determine input name and add it to source code", CURRENT_FUNCTION );
     }
 }
 
@@ -173,8 +181,6 @@ void MLOptimizer::SolveMultiCell( VectorVector& alpha, VectorVector& u, const Ve
             integral += _entropy->EntropyPrimeDual( dot( alphaRed, _reducedMomentBasis[idx_quad] ) ) * _weights[idx_quad];
         }
         alpha[idx_cell][0] = -log( integral );    // log trafo
-        // rescale alpha_0 ==> done by normalized MN Solver
-        // alpha[idx_cell][0] += log( u[idx_cell][0] );
     }
     // postprocessing depending on model mk
     // TextProcessingToolbox::PrintVectorVector( alpha );
