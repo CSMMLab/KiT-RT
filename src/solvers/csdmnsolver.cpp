13ceb4291a689e769483fa0eb4e1c59863350e7a
Jonas Kusch <jonas.kusch@kit.edu>
2022-04-14 09:57:02 +0200

    MN Solver now wrties txt file

--- src/solvers/csdmnsolver.cpp
+++ src/solvers/csdmnsolver.cpp
@@ -347,6 +347,15 @@ void CSDMNSolver::WriteVolumeOutput( unsigned idx_pseudoTime ) {
             }
         }
     }
+    if( idx_pseudoTime == _nEnergies - 2 ) {
+        std::ofstream out( _settings->GetOutputFile().append( ".txt" ) );
+        unsigned nx = _settings->GetNCells();
+
+        for( unsigned j = 0; j < nx; ++j ) {
+            out << _cellMidPoints[j][0] << " " << _cellMidPoints[j][1] << " " << _dose[j] << std::endl;
+        }
+        out.close();
+    }
 }
 
 double CSDMNSolver::NormPDF( double x, double mu, double sigma ) {
