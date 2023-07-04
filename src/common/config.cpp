f265e75334a4596552876706552760d38f22d744
Steffen Schotthöfer <steffen.schotthoefer@kit.edu>
2023-07-04 18:43:49 +0200

    change: default values for newton

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -289,12 +289,12 @@ void Config::SetConfigOptions() {
     /*! @brief Newton Optimizer Epsilon \n DESCRIPTION:  Convergencce Epsilon for Newton Optimizer \n DEFAULT 1e-3 \ingroup Config */
     AddDoubleOption( "NEWTON_EPSILON", _optimizerEpsilon, 0.001 );
     /*! @brief Max Iter Newton Optmizers \n DESCRIPTION: Max number of newton iterations \n DEFAULT 10 \ingroup Config */
-    AddUnsignedLongOption( "NEWTON_ITER", _newtonIter, 100 );
+    AddUnsignedLongOption( "NEWTON_ITER", _newtonIter, 1000 );
     /*! @brief Step Size Newton Optmizers \n DESCRIPTION: Step size for Newton optimizer \n DEFAULT 10 \ingroup Config */
-    AddDoubleOption( "NEWTON_STEP_SIZE", _newtonStepSize, 0.1 );
+    AddDoubleOption( "NEWTON_STEP_SIZE", _newtonStepSize, 1 );
     /*! @brief Max Iter for line search in Newton Optmizers \n DESCRIPTION: Max number of line search iter for newton optimizer \n DEFAULT 10
      * \ingroup Config */
-    AddUnsignedLongOption( "NEWTON_LINE_SEARCH_ITER", _newtonLineSearchIter, 100 );
+    AddUnsignedLongOption( "NEWTON_LINE_SEARCH_ITER", _newtonLineSearchIter, 1000 );
     /*! @brief Newton Fast mode \n DESCRIPTION:  If true, we skip the Newton optimizer for Quadratic entropy and set alpha = u \n DEFAULT false
      * \ingroup Config */
     AddBoolOption( "NEWTON_FAST_MODE", _newtonFastMode, false );
