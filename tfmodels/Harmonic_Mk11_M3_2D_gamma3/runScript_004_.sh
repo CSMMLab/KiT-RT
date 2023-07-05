33bc7a44919719dcebfd2e7efd3d081e91ea7d3b
Steffen <steffen.schotthoefer@kit.edu>
2023-07-05 15:34:40 +0200

    enhance: add M3 spherical harmonics models

---
+++ tfmodels/Harmonic_Mk11_M3_2D_gamma3/runScript_004_.sh
@@ -0,0 +1,22 @@
+python callNeuralClosure.py \
+--sampling=2 \
+--batch=256 \
+--curriculum=1 \
+--degree=3 \
+--epoch=1000 \
+--folder=Harmonic_Mk11_M3_2D_gamma3 \
+--loadModel=1 \
+--model=11 \
+--normalized=1 \
+--scaledOutput=0 \
+--decorrInput=1 \
+--objective=2 \
+--processingmode=1 \
+--spatialDimension=2 \
+--training=1 \
+--verbosity=2 \
+--networkwidth=300 \
+--networkdepth=3 \
+--basis=spherical_harmonics \
+--rotated=0 \
+--max_alpha_norm=1.5 \
