17b9d0ae21b44705c90bcca4e121220b01df883e
ScSteffen <steffen.schotthoefer@outlook.de>
2023-07-06 16:56:04 +0200

    added measure for inaccuraate nn cells

--- src/solvers/mnsolver_normalized.cpp
+++ src/solvers/mnsolver_normalized.cpp
@@ -33,35 +33,57 @@ MNSolverNormalized::MNSolverNormalized( Config* settings ) : MNSolver( settings 
 MNSolverNormalized::~MNSolverNormalized() {}
 
 void MNSolverNormalized::IterPreprocessing( unsigned idx_pseudotime ) {
-
-    // ------- Entropy closure Step ----------------
-#pragma omp parallel for
-    for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
-        _u0[idx_cell] = _sol[idx_cell][0];
-        _sol[idx_cell] /= _u0[idx_cell];    // assume _u0 > 0 always!!
-    }
     Vector alpha_norm_per_cell( _nCells, 0 );    // ONLY FOR DEBUGGING! THIS SLOWS DOWN THE CODE
 
-    _optimizer->SolveMultiCell( _alpha, _sol, _momentBasis, alpha_norm_per_cell );    // Newton for the first few iterations
-
-    int norm_err = 0.0;
-    // Check if solution is close to reconstruction, if not, apply newton optimizer
-#pragma omp parallel for
-    for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
-        _optimizer2->ReconstructMoments( _sol2[idx_cell], _alpha[idx_cell], _momentBasis );
-        //_optimizer2->ReconstructMoments( _sol2[idx_cell], _alpha[idx_cell], _momentBasis );
-        if( blaze::norm( _sol2[idx_cell] - _sol[idx_cell] ) / blaze::norm( _sol[idx_cell] ) > _settings->GetNewtonOptimizerEpsilon() ) {
-            _optimizer2->Solve( _alpha[idx_cell], _sol[idx_cell], _momentBasis, idx_cell );    // Newton postprocessor
-            norm_err++;
+    //if (idx_pseudotime < 1600){
+    //    // preprocessor
+    //    _optimizer3->SolveMultiCell( _alpha, _sol, _momentBasis, alpha_norm_per_cell );    // Newton for the first few iterations
+//
+    //    #pragma omp parallel for
+    //    for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
+    //        for( unsigned idx_quad = 0; idx_quad < _nq; idx_quad++ ) {
+    //            // compute the kinetic density at all grid cells
+    //            _kineticDensity[idx_cell][idx_quad] = _entropy->EntropyPrimeDual( blaze::dot( _alpha[idx_cell], _momentBasis[idx_quad] ) );
+    //        }
+    //    }
+    //}
+    //else
+    {
+        //std::cout << "Start Network based closure";
+        //if (idx_pseudotime==1600){
+        //    #pragma omp parallel for
+        //    for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
+        //         _sol[idx_cell][0] = max(1e-10, _sol[idx_cell][0]);
+        //    }
+        //}
+        // ------- Entropy closure Step ----------------
+    #pragma omp parallel for
+        for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
+            _u0[idx_cell] = _sol[idx_cell][0];
+            _sol[idx_cell] /= _u0[idx_cell];    // assume _u0 > 0 always!!
         }
-        // Dynamic closure
-        alpha_norm_per_cell[idx_cell] = 0.0;
-        for( unsigned idx_sys = 1; idx_sys < _nSystem; idx_sys++ ) {
-            alpha_norm_per_cell[idx_cell] += _alpha[idx_cell][idx_sys] * _alpha[idx_cell][idx_sys];
-        }
-    }
-    std::cout << "Number of inaccurate cells: " << norm_err << "\n";
 
+        
+        _optimizer->SolveMultiCell( _alpha, _sol, _momentBasis, alpha_norm_per_cell );    // Newton for the first few iterations
+
+        int norm_err = 0.0;
+        // Check if solution is close to reconstruction, if not, apply newton optimizer
+    #pragma omp parallel for
+        for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
+            _optimizer2->ReconstructMoments( _sol2[idx_cell], _alpha[idx_cell], _momentBasis );
+            //_optimizer2->ReconstructMoments( _sol2[idx_cell], _alpha[idx_cell], _momentBasis );
+            if( blaze::norm( _sol2[idx_cell] - _sol[idx_cell] ) / blaze::norm( _sol[idx_cell] ) > _settings->GetNewtonOptimizerEpsilon() ) {
+                //_optimizer2->Solve( _alpha[idx_cell], _sol[idx_cell], _momentBasis, idx_cell );    // Newton postprocessor
+                norm_err++;
+            }
+            // Dynamic closure
+            alpha_norm_per_cell[idx_cell] = 0.0;
+            for( unsigned idx_sys = 1; idx_sys < _nSystem; idx_sys++ ) {
+                alpha_norm_per_cell[idx_cell] += _alpha[idx_cell][idx_sys] * _alpha[idx_cell][idx_sys];
+            }
+        }
+        std::cout << "Number of inaccurate cells: " << norm_err << "\n";
+    
     // ------- Solution reconstruction step ----
 #pragma omp parallel for
     for( unsigned idx_cell = 0; idx_cell < _nCells; idx_cell++ ) {
@@ -85,6 +107,7 @@ void MNSolverNormalized::IterPreprocessing( unsigned idx_pseudotime ) {
             ComputeRealizableSolution( idx_cell );
         }
         _sol[idx_cell] *= _u0[idx_cell];
+    }
     }
 
     // ------ Compute slope limiters and cell gradients ---
