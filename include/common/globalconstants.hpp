5d40cba9fd3cffbd778a1e516a56564ec225a9da
Steffen Schotthoefer <steffen.schotthoefer@outlook.de>
2023-12-22 13:51:44 -0500

    added green line scalar output

--- include/common/globalconstants.hpp
+++ include/common/globalconstants.hpp
@@ -185,7 +185,8 @@ enum SCALAR_OUTPUT {
     TOTAL_PARTICLE_ABSORPTION_VERTICAL,
     TOTAL_PARTICLE_ABSORPTION_HORIZONTAL,
     PROBE_MOMENT_TIME_TRACE,
-    VAR_ABSORPTION_GREEN
+    VAR_ABSORPTION_GREEN,
+    VAR_ABSORPTION_GREEN_LINE
 };
 
 inline std::map<std::string, SCALAR_OUTPUT> ScalarOutput_Map{ { "ITER", ITER },
@@ -203,7 +204,8 @@ inline std::map<std::string, SCALAR_OUTPUT> ScalarOutput_Map{ { "ITER", ITER },
                                                               { "TOTAL_PARTICLE_ABSORPTION_VERTICAL", TOTAL_PARTICLE_ABSORPTION_VERTICAL },
                                                               { "TOTAL_PARTICLE_ABSORPTION_HORIZONTAL", TOTAL_PARTICLE_ABSORPTION_HORIZONTAL },
                                                               { "PROBE_MOMENT_TIME_TRACE", PROBE_MOMENT_TIME_TRACE },
-                                                              { "VAR_ABSORPTION_GREEN", VAR_ABSORPTION_GREEN } };
+                                                              { "VAR_ABSORPTION_GREEN", VAR_ABSORPTION_GREEN },
+                                                              { "VAR_ABSORPTION_GREEN_LINE", VAR_ABSORPTION_GREEN_LINE } };
 
 // Spherical Basis Name
 enum SPHERICAL_BASIS_NAME { SPHERICAL_HARMONICS, SPHERICAL_MONOMIALS, SPHERICAL_MONOMIALS_ROTATED };
