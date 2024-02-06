ee543122b79265a14728bece42a4e4290a4c1273
Steffen Schotthoefer <steffensch@baseline-login1.ccs.ornl.gov>
2024-02-06 13:34:04 -0500

    added output verbosity

--- examples_hpc/lattice/mesh/make_lattice_mesh.sh
+++ examples_hpc/lattice/mesh/make_lattice_mesh.sh
@@ -1,7 +1,9 @@
 #rm lattice_n20.su2 lattice_n20.con 
 #rm lattice_n40.su2 lattice_n40.con 
-rm lattice_n80.su2 lattice_n80.con
+#rm lattice_n80.su2 lattice_n80.con
+rm lattice_n160.su2 lattice_n160.con
 
 #gmsh lattice_rectangular_n20.geo -2 -format su2 -save_all -o lattice_n20.su2
 #gmsh lattice_rectangular_n40.geo -2 -format su2 -save_all -o lattice_n40.su2
-gmsh lattice_rectangular_n80.geo -2 -format su2 -save_all -o lattice_n80.su2
+#gmsh lattice_rectangular_n80.geo -2 -format su2 -save_all -o lattice_n80.su2
+gmsh lattice_rectangular_n160.geo -2 -format su2 -save_all -o lattice_n160.su2
